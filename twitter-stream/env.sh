f5801deba42f576d90660688912c6dd6d5b51e20
dm-istomin <dmi.istomin@gmail.com>
2015-10-18 00:31:52 -0400

    add config for setting Twitter API credentials

---
+++ twitter-stream/env.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Set these variables or the application will not be able to access the Twitter API!
+
+# export CONSUMER_KEY=''
+# export CONSUMER_SECRET=''
+# export ACCESS_TOKEN_KEY=''
+# export ACCESS_TOKEN_SECRET=''
