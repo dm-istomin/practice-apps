4437394ddf798416953b9bda7bf2c03151e29409
N/A <malct_@NAs-MacBook-Pro-2.local>
2015-11-12 14:48:01 -0500

    add a simple message

--- simple-rails-react-example/app/assets/javascripts/components/hello_world.es6.jsx
+++ simple-rails-react-example/app/assets/javascripts/components/hello_world.es6.jsx
@@ -1,10 +1,13 @@
-console.log('hello_world.es6.jsx is loaded!');
 class HelloMessage extends React.Component {
   render () {
     const welcome = "Hello, ";
     return (
       <div>
         <h3>{welcome} {this.props.name} !</h3>
+        <p>
+        If you're seeing a message above this line, it means that everything loaded
+        correctly.
+        </p>
       </div>
     );
   }
