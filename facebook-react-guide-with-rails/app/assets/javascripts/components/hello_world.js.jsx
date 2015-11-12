a8af98da4bffe8bfeab13277becac7bc5739f45c
Dmitriy Istomin <dmi.istomin@gmail.com>
2015-11-12 18:28:26 -0500

    modify the hello world component to be more informative

--- facebook-react-guide-with-rails/app/assets/javascripts/components/hello_world.js.jsx
+++ facebook-react-guide-with-rails/app/assets/javascripts/components/hello_world.js.jsx
@@ -3,6 +3,7 @@ var HelloWorld = React.createClass({
   render: function() {
     return (
       <p>
+        <em>Type your name. Notice that only the time gets redrawn:</em><br/><br/><br/>
         Hello, <input type="text" placeholder="Your name here" /> !<br/>
         It is <b>{ this.props.date.toTimeString() }</b>
       </p>
